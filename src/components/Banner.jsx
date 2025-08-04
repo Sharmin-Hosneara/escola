c648d467ff56e6440b3c9874bd53d8abda12760e
Sharmin-Hosneara <sultansharmin30@gmail.com>
2025-08-04 16:24:10 +0600

    logo update

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='py-20 bg-blue-100'>
+        <p>Unlock your potential through education</p>
+    </div>
+  )
+}
+
+export default Banner
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import style from '@/components/Topbar.module.css'
+import Image from 'next/image'
 import Link from 'next/link'
+import logo from "../../public/Logo.png"
 const Navbar = () => {
   return (
     <nav className={`py-4 px-11 bg-(--color-navbar) relative ${style.topbar}`}>
       <div className='flex justify-center items-center'>
         <div className='z-10 flex justify-start items-center'>
           <Link href="#">
-          <img src="logo.png" alt="Logo" />
+          <Image src={logo} alt="Logo" width={200} height={80} />
           </Link>
         </div>
           <ul className='pl-[155px] flex items-center gap-[25px] flex-1'>
